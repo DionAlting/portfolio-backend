5a48a9b008b543ac36c49a5198d58d5e89475e6d
Dion Alting <dion_a@live.nl>
2021-04-06 21:14:00 +0200

    Created user model and migration files

---
+++ migrations/20210406190423-create-user.js
@@ -0,0 +1,54 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      passwordHash: {
+        type: Sequelize.STRING,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+      },
+      avatar: {
+        type: Sequelize.STRING,
+      },
+      studyAssociation: {
+        type: Sequelize.INTEGER,
+      },
+      isAdmin: {
+        type: Sequelize.BOOLEAN,
+      },
+      isStudyAssociation: {
+        type: Sequelize.BOOLEAN,
+      },
+      isBlocked: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
