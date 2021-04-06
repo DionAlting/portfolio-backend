208b56c3145c95d88df775576b7c014673392762
Dion Alting <dion_a@live.nl>
2021-04-06 21:27:11 +0200

    Added contraints and default values to user migration

--- migrations/20210406190423-create-user.js
+++ migrations/20210406190423-create-user.js
@@ -10,33 +10,46 @@ module.exports = {
       },
       email: {
         type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       passwordHash: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       firstName: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       lastName: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       displayName: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       avatar: {
         type: Sequelize.STRING,
       },
       studyAssociation: {
         type: Sequelize.INTEGER,
+        allowNull: false,
       },
       isAdmin: {
         type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       isStudyAssociation: {
-        type: Sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       isBlocked: {
-        type: Sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
