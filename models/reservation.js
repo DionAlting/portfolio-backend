9ae925690ce7155c62e6e983a9166cae66956ca2
Dion Alting <dion_a@live.nl>
2021-04-07 14:35:27 +0200

    Added reservation relations

--- models/reservation.js
+++ models/reservation.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      reservation.belongsTo(models.reservationDate);
+      reservation.belongsTo(models.user);
+      reservation.hasMany(models.stamp);
     }
   }
   reservation.init(
