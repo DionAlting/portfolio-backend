9175fdb0c9f6d7beba278295ace9f0283eb4bd81
Dion Alting <dion_a@live.nl>
2021-04-09 16:39:00 +0200

    Updated models for relations

--- models/studyassociation.js
+++ models/studyassociation.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      studyAssociation.belongsTo(models.user);
+      studyAssociation.hasMany(models.user);
     }
   }
   studyAssociation.init(
