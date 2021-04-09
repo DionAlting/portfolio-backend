3344a33cda91741e4dce37225299b8277402b381
Dion Alting <dion_a@live.nl>
2021-04-09 16:37:50 +0200

    Login and register endpoints

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
 require("dotenv").config();
+
+const authRouter = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,5 +13,7 @@ app.use(helmet());
 app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
