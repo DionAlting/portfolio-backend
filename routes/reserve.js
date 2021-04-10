4a8d1af84a2173c004b15dd10b4b4261f70e14bf
Dion Alting <dion_a@live.nl>
2021-04-10 14:18:37 +0200

    Created post reservation endpoint

--- routes/reserve.js
+++ routes/reserve.js
@@ -1,4 +1,8 @@
 const { Router } = require("express");
+const { Op } = require("sequelize");
+
+const authMiddleware = require("../auth/middleware").auth;
+
 const Reservation = require("../models/").reservation;
 const ReservationDate = require("../models/").reservationDate;
 
@@ -14,7 +18,7 @@ router.get("/", async (req, res) => {
     });
 
     if (!allReservationDates) {
-      return res.status(400).send({
+      return res.status(404).send({
         message: "No reservation dates found",
       });
     }
@@ -22,6 +26,74 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.post("/:dateId", authMiddleware, async (req, res) => {
+  try {
+    const { dateId } = req.params;
+    const { date } = req.body;
+    const { id, isAdmin } = req.user;
+
+    const reservationDate = await ReservationDate.findByPk(dateId);
+    if (!reservationDate) {
+      return res.status(404).send({ message: "Date does not exist" });
+    }
+
+    if (req.body.reservationDetails.length > reservationDate.maxPerParty) {
+      return res
+        .status(400)
+        .json({ error: `Max ${reservationDate.maxPerParty} per party.` });
+    }
+
+    const hasReserved = await Reservation.findOne({
+      where: {
+        [Op.and]: [{ dateId: reservationDate.id }, { userId: id }],
+      },
+    });
+
+    if (hasReserved && !isAdmin) {
+      return res
+        .status(400)
+        .send({ message: "You already made a reservation for this day" });
+    }
+    const reservationDetails = req.body.reservationDetails;
+
+    const insert = reservationDetails.map((item) => ({
+      dateId: reservationDate.id,
+      userId: id,
+      ...item,
+    }));
+
+    await Reservation.bulkCreate(insert);
+
+    const userReservations = await Reservation.findAll({
+      where: {
+        [Op.and]: [
+          { isCanceled: false },
+          { isCheckedOut: false },
+          { userId: id },
+        ],
+      },
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+      include: [
+        {
+          model: ReservationDate,
+          attributes: ["date"],
+        },
+      ],
+      order: [[ReservationDate, "date", "ASC"]],
+    });
+
+    return res.status(200).send({
+      message: "Reservation created successfully!",
+      userReservations,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(400)
+      .send({ message: `Something went wrong, sorry`, error });
   }
 });
 
