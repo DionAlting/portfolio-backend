327431fc63b0f7b84b10937a83026acf7cc89df8
Dion Alting <dion_a@live.nl>
2021-04-12 14:34:42 +0200

    Added more checks to reservation

--- routes/reserve.js
+++ routes/reserve.js
@@ -32,18 +32,26 @@ router.get("/dates", async (req, res) => {
 router.post("/:dateId", authMiddleware, async (req, res) => {
   try {
     const { dateId } = req.params;
-    const { date } = req.body;
     const { id, isAdmin } = req.user;
+    const { reservationDetails, comment } = req.body;
+
+    const partySize = req.body.reservationDetails.length;
 
     const reservationDate = await ReservationDate.findByPk(dateId);
     if (!reservationDate) {
       return res.status(404).send({ message: "Date does not exist" });
     }
 
-    if (req.body.reservationDetails.length > reservationDate.maxPerParty) {
+    if (reservationDate.bookedSeats + partySize > reservationDate.maxSeats) {
+      return res.status(400).send({
+        message: `Max amount of reservations (${reservationDate.maxSeats}) for this day exceeded`,
+      });
+    }
+
+    if (partySize > reservationDate.maxPerParty) {
       return res
         .status(400)
-        .json({ error: `Max ${reservationDate.maxPerParty} per party.` });
+        .send({ message: `Max ${reservationDate.maxPerParty} per party.` });
     }
 
     const hasReserved = await Reservation.findOne({
@@ -57,11 +65,11 @@ router.post("/:dateId", authMiddleware, async (req, res) => {
         .status(400)
         .send({ message: "You already made a reservation for this day" });
     }
-    const reservationDetails = req.body.reservationDetails;
 
     const insert = reservationDetails.map((item) => ({
       dateId: reservationDate.id,
       userId: id,
+      comment: comment,
       ...item,
     }));
 
@@ -84,6 +92,7 @@ router.post("/:dateId", authMiddleware, async (req, res) => {
       ],
       order: [[ReservationDate, "date", "ASC"]],
     });
+    await reservationDate.increment("bookedSeats", { by: partySize });
 
     return res.status(200).send({
       message: "Reservation created successfully!",
@@ -91,18 +100,21 @@ router.post("/:dateId", authMiddleware, async (req, res) => {
     });
   } catch (error) {
     console.log(error.message);
-    return res
-      .status(400)
-      .send({ message: `Something went wrong, sorry`, error });
+    return res.status(400).send({ message: `Something went wrong, sorry` });
   }
 });
 
 router.put("/:reservationId", authMiddleware, async (req, res) => {
   try {
     const { reservationId } = req.params;
+    const { dateId } = req.body;
     const { id } = req.user;
 
     const reservation = await Reservation.findByPk(reservationId);
+
+    const reservationDate = await ReservationDate.findOne({
+      where: { id: dateId },
+    });
 
     if (!reservation) {
       return res.status(404).send({ message: "Reservation does not exist" });
@@ -115,6 +127,8 @@ router.put("/:reservationId", authMiddleware, async (req, res) => {
     const updatedReservation = await reservation.update({
       isCanceled: !reservation.isCanceled,
     });
+
+    await reservationDate.decrement("bookedSeats");
 
     return res.status(200).send({
       message: "Reservation canceled successfully!",
