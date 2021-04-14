c3ef38b17ef18a81f477300f598124a78137dd52
Dion Alting <dion_a@live.nl>
2021-04-14 15:05:14 +0200

    Created update profile endpoint

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,48 @@ const { Op } = require("sequelize");
 const authMiddleware = require("../auth/middleware").auth;
 
 const User = require("../models/").user;
+const StudyAssociation = require("../models/").studyAssociation;
 
 const router = new Router();
+
+router.patch("/:userId", authMiddleware, async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { userId } = req.params;
+    const userById = await User.findByPk(userId);
+
+    if (!userById) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    if (!userById.id === id) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    const updatedUser = await userById.update(req.body.values);
+
+    const cleanUpdatedUser = await User.findOne({
+      where: { id: updatedUser.id },
+      attributes: {
+        exclude: ["passwordHash", "studyAssociationId", "updatedAt"],
+      },
+      include: [
+        {
+          model: StudyAssociation,
+          attributes: ["name", "id"],
+        },
+      ],
+    });
+
+    return res
+      .status(200)
+      .send({ message: "User updated successfully!", cleanUpdatedUser });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
 
 module.exports = router;
