b641491a5c3bafdac14350ab501d7708d697cbfd
Dion Alting <dion_a@live.nl>
2021-04-29 17:05:52 +0200

    Fixed ssl error

--- config/config.js
+++ config/config.js
@@ -15,5 +15,10 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
