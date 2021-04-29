22a6b7425ec6a668f4c6868ab30e6efdfe0aca56
Dion Alting <dion_a@live.nl>
2021-04-29 14:38:47 +0200

    Added increment/decrement endpoints

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const { Op } = require("sequelize");
 const authMiddleware = require("../auth/middleware").auth;
 const isAdmin = require("../auth/middleware").isAdmin;
 
@@ -8,6 +8,7 @@ const User = require("../models/").user;
 const StudyAssociation = require("../models/").studyAssociation;
 const Reservation = require("../models/").reservation;
 const ReservationDate = require("../models/").reservationDate;
+const Stamp = require("../models/").stamp;
 
 const router = new Router();
 
@@ -17,6 +18,10 @@ router.get("/reservations", authMiddleware, isAdmin, async (req, res) => {
       include: [
         {
           model: Reservation,
+          where: {
+            [Op.and]: [{ isCanceled: false }, { isCheckedOut: false }],
+          },
+          order: [["userId", "ASC"]],
           attributes: { exclude: ["createdAt", "updatedAt"] },
         },
       ],
@@ -43,7 +48,6 @@ router.put(
   async (req, res) => {
     try {
       const { reservationId } = req.params;
-      const { id } = req.user;
       const reservation = await Reservation.findByPk(reservationId);
 
       if (!reservation) {
@@ -56,8 +60,12 @@ router.put(
       );
 
       if (reservationDate.isStampable) {
-        await Stamp.create({ userId: id, reservationId: reservation.id });
+        await Stamp.create({
+          userId: reservation.userId,
+          reservationId: reservation.id,
+        });
       }
+      await reservationDate.decrement("bookedSeats");
 
       await reservation.update({ isCheckedOut: !reservation.isCheckedOut });
 
@@ -85,10 +93,64 @@ router.put(
       }
 
       await reservation.update({ isCheckedOut: !reservation.isCanceled });
+      const reservationDate = await ReservationDate.findByPk(
+        reservation.dateId
+      );
 
+      await reservationDate.decrement("bookedSeats");
       return res
         .status(200)
         .send({ message: "Reservation canceled successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({ message: "Something went wrong, sorry" });
+    }
+  }
+);
+
+router.put(
+  "/:reservationId/increment",
+  authMiddleware,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const { reservationId } = req.params;
+      const reservation = await Reservation.findByPk(reservationId);
+
+      if (!reservation) {
+        return res.status(404).send({
+          message: "Reservation not found",
+        });
+      }
+
+      await reservation.increment("coins");
+
+      return res.status(200).send({ message: "Incremented successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({ message: "Something went wrong, sorry" });
+    }
+  }
+);
+
+router.put(
+  "/:reservationId/decrement",
+  authMiddleware,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const { reservationId } = req.params;
+      const reservation = await Reservation.findByPk(reservationId);
+
+      if (!reservation) {
+        return res.status(404).send({
+          message: "Reservation not found",
+        });
+      }
+
+      await reservation.decrement("coins");
+
+      return res.status(200).send({ message: "Decremented successfully" });
     } catch (error) {
       console.log(error);
       return res.status(400).send({ message: "Something went wrong, sorry" });
